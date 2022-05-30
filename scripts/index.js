70ba95cd8a15b667b78e64497861b758004db28e
Nazar Samborskyj <nazarij.sambors.020@pslib.cz>
2022-05-30 19:26:14 +0200

    edited files

---
+++ scripts/index.js
@@ -0,0 +1,16 @@
+let slogan = document.getElementById("slogan");
+var limit = Math.max( document.body.scrollHeight, document.body.offsetHeight, 
+    document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight );
+var width = this.window.innerWidth;
+window.addEventListener("scroll", function(){
+    var value  = window.scrollY; 
+    if(width >= 1200){
+        if(value < 1000) slogan.style.top = 2495 + -value * 1.25 + "px"; 
+    } 
+    if(width < 1200 && width >= 1036){
+        if(value < 925) slogan.style.top = 1950 + -value + "px";  
+    }
+    if(width < 1036 && width > 992){
+        if(value < 860) slogan.style.top = 1900 + -value + "px";  
+    }
+})
